3dee0969d3c2dee9e35243bf0729ee66d9756b3b
Igor Kondrashov <reid09@mail.ru>
2020-11-28 18:53:55 +0300

    shema

--- api/db/schema.rb
+++ api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20201128143939) do
+ActiveRecord::Schema.define(version: 20201128154904) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 20201128143939) do
     t.jsonb "stages"
     t.string "file"
     t.string "doc_app"
+    t.integer "count_likes", default: 0
     t.index ["user_id"], name: "index_applications_on_user_id"
   end
 
