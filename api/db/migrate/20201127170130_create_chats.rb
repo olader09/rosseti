114138d7a27c16b8deeef7a373488514c7c5f831
Igor Kondrashov <reid09@mail.ru>
2020-11-28 14:15:58 +0300

    fix

--- api/db/migrate/20201127170130_create_chats.rb
+++ api/db/migrate/20201127170130_create_chats.rb
@@ -3,6 +3,7 @@ class CreateChats < ActiveRecord::Migration[5.1]
     create_table :chats do |t|
       t.bigint :application_id, null: false
       t.index :application_id
+      t.timestamps
     end
   end
 end
