e038e845d6312021b4c629fd202b9ab29f0c102c
Igor Kondrashov <reid09@mail.ru>
2020-11-28 13:22:57 +0300

    fixing

--- api/db/migrate/20201127165813_create_aplications.rb
+++ api/db/migrate/20201127165813_create_aplications.rb
@@ -1,6 +1,6 @@
-class CreateApplications < ActiveRecord::Migration[5.1]
+class CreateAplications < ActiveRecord::Migration[5.1]
   def change
-    create_table :applications do |t|
+    create_table :aplications do |t|
       t.belongs_to :user, class_name: "user", foreign_key: "user_id"
       t.string :title, null: false
       t.text :text, null: false
