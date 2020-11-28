7ca7e933a3692697c0061a3f4ecc2c373459f1d3
Igor Kondrashov <reid09@mail.ru>
2020-11-29 02:44:49 +0300

    fix

--- api/app/controllers/applications_controller.rb
+++ api/app/controllers/applications_controller.rb
@@ -91,6 +91,8 @@ class ApplicationsController < APIBaseController
           tie_breaker: 0.5
         }
       }}).to_a
+      p similars
+      p similars.empty?
       if similars.empty?
         @application.update(uniqueness: 100) 
         render status: :no_content
