92cffa68d3484514bb189db364719f086961389f
Igor Kondrashov <reid09@mail.ru>
2020-11-29 04:54:15 +0300

    edit score

--- api/app/controllers/applications_controller.rb
+++ api/app/controllers/applications_controller.rb
@@ -129,7 +129,7 @@ class ApplicationsController < APIBaseController
         total_score = 0.0
         scores.each {|score| total_score += score}
         p total_score
-        total_score = (total_score * 100 / 76)
+        total_score = (total_score * 100 / 80)
         render json: {"uniqueness": total_score}
       end
   end
