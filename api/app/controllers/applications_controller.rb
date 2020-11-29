fa92546ee4625ee21d29440cc0c57bbbb938921f
Igor Kondrashov <reid09@mail.ru>
2020-11-29 03:33:57 +0300

    redo

--- api/app/controllers/applications_controller.rb
+++ api/app/controllers/applications_controller.rb
@@ -120,7 +120,7 @@ class ApplicationsController < APIBaseController
   end
 
   def create_application_params
-    params.permit(
+    params.required(:application).permit(
       *default_application_fields
     )
   end
